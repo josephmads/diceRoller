89678bf7b9e7f8c11b6878b188b21b7084c545a7
Joseph Madsen <josephmads@protonmail.com>
2022-06-24 16:09:06 -0500

    Updated 'roll_again' var

--- diceRoller.py
+++ diceRoller.py
@@ -1,10 +1,18 @@
+# A simple dice rolling script that allows for any size and quanitiy of dice.
+
 from random import randint as roll
 import time
 
 def diceroller(size, quantity):
-  for n in range(quantity):
-    dice = roll(1, size)
-    print(dice)
+    """Takes input of size and quantity to generate a pseudo-random dice roll.
+
+    Args:
+        size (int): The number of sides the dice will have.
+        quantity (int): The number of dice rolled.
+    """
+    for n in range(quantity):
+        dice = roll(1, size)
+        print(dice)
 
 setup_dice = True
 
@@ -20,13 +28,22 @@ while setup_dice == True:
         diceroller(d_size, d_quantity)
         
         roll_again = input("Roll [a]gain, Roll [n]ew dice, or [q]uit?: ")
+        roll_again = roll_again.lower()
+
         if roll_again == "a":
             continue
+
         if roll_again == "n":
             rolling = False
             continue
+
         if roll_again == "q":
+            print("Goodbye.")
             rolling = False
             setup_dice = False
             break
 
+        else:
+            print("Invalid Input")
+            rolling = False
+            continue
